1b427b5b3b65426bd84870994a1136ce744b139a
HN67 <31172532+HN67@users.noreply.github.com>
2017-12-22 18:21:08 -0700

    Delete SriteBotInfo.py

--- SriteBotDir/SriteBotInfo.py
+++
@@ -1,3 +0,0 @@
-'''Private file for sensitive information'''
-bot_id = "MzQ4NjUzNjAwMzQ1NDIzODcz.DOZ67w.w5c2_MxbiOLK7mQfvnTHbf75lQA"
-directory = "C:\\Users\\Allard Family\\Desktop\\Stuff\\PythonWorkspace\\SriteBotDir"
