755efe410eafab644785e840663335dd2c459618
HN67 <hn67allardkids@gmail.com>
2020-03-22 23:46:02 -0600

    Align timing with pylint

--- cogs/timing.py
+++ cogs/timing.py
@@ -1,19 +1,22 @@
-# Timing commands cog for SriteBot
-import discord
-from discord.ext import commands
+"""Timing commands cog for SriteBot"""
 
 # Import core librarys
 import asyncio
 import time
 
+# Import discord.py
+import discord
+from discord.ext import commands
+
 # Import core
-from core import *
+import core
 
 # Import config
 import config
 
 # Cog class
 class Timing(commands.Cog):
+    """SriteBot Timing Cog"""
 
     # Init to reference bot
     def __init__(self, bot):
@@ -23,12 +26,13 @@ class Timing(commands.Cog):
     @commands.command()
     async def surprise(self, ctx, delay: int, *, message: str):
         '''Echos the message after delay seconds'''
-        debug_info("Surprise message",message)
+        core.debug_info("Surprise message", message)
         await asyncio.sleep(delay)
         await ctx.send(message)
 
     @surprise.error
     async def surprise_handler(self, ctx, error):
+        """Handles surprise command errors"""
         if isinstance(error, discord.ext.commands.BadArgument):
             await ctx.send("Please use whole numbers")
         else:
@@ -44,22 +48,22 @@ class Timing(commands.Cog):
         message = await ctx.send("```Timer: {0}```".format(left))
         # Tick down through qued remaining time, editing message
         while left > 0:
-            await message.edit(content = "```Timer: {0}```".format(left))
+            await message.edit(content="```Timer: {0}```".format(left))
             left -= config.time.interval
             await asyncio.sleep(config.time.interval)
         # Finish the timer message
-        await message.edit(content = "```Timer: Done```")
+        await message.edit(content="```Timer: Done```")
         # Mention original command author
         await ctx.send("Timer Finished {0}".format(ctx.author.mention))
 
-    @commands.command(name = "time")
+    @commands.command(name="time")
     async def _time(self, ctx):
         """Times how long it takes you to respond with 'stop'"""
         # Save current time
         current = time.time()
 
-        await srite_send(ctx.channel, "Waiting for 'stop' within 10s")
-        
+        await core.srite_send(ctx.channel, "Waiting for 'stop' within 10s")
+
         # Define check (what msg to wait for)
         def check(msg):
             return (msg.content == "stop" and msg.channel == ctx.channel
@@ -67,12 +71,12 @@ class Timing(commands.Cog):
 
         # Wait for a response
         try:
-            await self.bot.wait_for("message", check = check,
-                                    timeout = config.time.timeout)
+            await self.bot.wait_for("message", check=check,
+                                    timeout=config.time.timeout)
 
         # Timeout clause
         except asyncio.TimeoutError:
-            await ctx.send(embed = srite_msg(
+            await ctx.send(embed=core.srite_msg(
                 f"Timeout at {config.time.timeout} seconds"))
 
         # Response found
@@ -80,8 +84,9 @@ class Timing(commands.Cog):
             # Find change in time
             change = time.time() - current
             # Send msg
-            await ctx.send(embed = srite_msg(f"Responded in {change} seconds"))
-    
+            await ctx.send(embed=core.srite_msg(f"Responded in {change} seconds"))
+
 # Function to load cog
 def setup(bot):
+    """Loads timing cog"""
     bot.add_cog(Timing(bot))
