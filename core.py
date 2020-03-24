9f1e015672dd8d871a7fb93c5ebf6817c21dd78e
HN67 <hn67allardkids@gmail.com>
2020-03-24 00:16:18 -0600

    Cleanup core file

--- core.py
+++ core.py
@@ -1,39 +1,39 @@
-# SriteBot core functions file
+"""SriteBot core functions file"""
+
 # Import discord
 import discord
-from discord.ext import commands
-
-# Import core libararies
-import json
 
 # Import config
 import config
 
 # Makes sure the guild is equipped to deal with economy (e.g. emoji)
 async def sriteEmoji(guild: discord.Guild):
+    """Attempts to return a SriteCoin emoji"""
 
     for e in guild.emojis:
         if e.name == "sritecoin":
             return e
 
-    else:
+    else: #pylint: disable=useless-else-on-loop
         try:
             with open("resources/sritecoin.png", "rb") as i:
-                emoji = await guild.create_custom_emoji(name="sritecoin",
-                                                        image = i.read())
+                emoji = await guild.create_custom_emoji(
+                    name="sritecoin",
+                    image=i.read()
+                )
             return emoji
-        except discord.errors.Forbidden: 
+        except discord.errors.Forbidden:
             return "SC (No emoji perms)"
 
 # Returns an embed wrapping the text
 def srite_msg(value: str):
-
-    return discord.Embed(color = config.bot.color, description = value)
+    """Returns a customized embed with the given message"""
+    return discord.Embed(color=config.bot.color, description=value)
 
 # Sends an embedded msg to channel
-async def srite_send(channel: discord.TextChannel, message: str):
-
-    await channel.send(embed = srite_msg(message))
+async def srite_send(channel: discord.abc.Messageable, message: str):
+    """Sends the message as an embed"""
+    return await channel.send(embed=srite_msg(message))
 
 # Debug method
 def debug_info(*messages):
@@ -43,4 +43,3 @@ def debug_info(*messages):
         print(line)
     # Prints finishing line
     print("-----")
-
