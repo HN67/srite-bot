a3a93aad5e979c2e6ade2d533149e9f695e84b2c
HN67 <hn67allardkids@gmail.com>
2020-04-19 23:27:17 -0600

    Add reset mod command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -14,8 +14,14 @@ class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.command()
+    @commands.group()
     @commands.has_permissions(administrator=True)
+    async def mod(self, ctx: commands.Context) -> None:
+        """Moderation command group"""
+        if ctx.invoked_subcommand is None:
+            await core.srite_send(ctx, "Please specify a subcommand")
+
+    @mod.command()
     async def prune(self, ctx: commands.Context, number: int) -> None:
         """Prunes messages from the channel"""
         core.debug_info("Running prune command")
@@ -25,17 +31,32 @@ class Moderation(commands.Cog):
         await core.srite_send(ctx, f"Successfully pruned {number} messages")
 
     @prune.error
-    async def prune_handler(self, ctx: commands.Context, error: commands.CommandError) -> None:
+    async def prune_handler(
+        self, ctx: commands.Context, error: commands.CommandError
+    ) -> None:
         """Error handler for prune command"""
         core.debug_info("Running prune handler", error)
         if isinstance(error, commands.MissingRequiredArgument):
-            await core.srite_send(ctx, "Specify the number of messages to prune. use *prune <num>")
+            await core.srite_send(
+                ctx, "Specify the number of messages to prune. use *prune <num>"
+            )
         if isinstance(error, commands.BadArgument):
             await core.srite_send(
-                ctx, "Specify the number of messages to prune as a number. use *prune <num>"
+                ctx,
+                "Specify the number of messages to prune as a number. use *prune <num>",
             )
         elif isinstance(error, commands.MissingPermissions):
-            await core.srite_send(ctx, "Must be an admin to use this command. Nice try.")
+            await core.srite_send(
+                ctx, "Must be an admin to use this command. Nice try."
+            )
+
+    @mod.command()
+    async def reset(self, ctx: commands.Context, message: discord.Message) -> None:
+        """Cleans everything in the channel after the given message (usually by ID)"""
+        core.debug_info("Running prune command")
+        channel: discord.TextChannel = ctx.channel
+        async for message in channel.history(after=message):
+            await message.delete()
 
 
 # Function to add cog
