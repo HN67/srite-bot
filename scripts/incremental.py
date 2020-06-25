77f6294fe43d05e6fd33bd869aea58331ecdb022
HN67 <hn67allardkids@gmail.com>
2020-06-24 22:44:42 -0600

    Design factory class and basic recipes

--- scripts/incremental.py
+++ scripts/incremental.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 from collections import Counter
 import enum
 import dataclasses
-from typing import Mapping, MutableMapping, Protocol, TypeVar
+from typing import Mapping, MutableMapping, MutableSequence, Protocol, TypeVar
 
 
 # TASKS:
@@ -82,7 +82,7 @@ class Inventory(
     """A enhanced mutable mapping that also supports comparisions and addition/subtraction"""
 
 
-class EnhancedCounter(Counter, Inventory):  # pylint: disable=abstract-method
+class Bag(Inventory, Counter):  # pylint: disable=abstract-method
     """A enhanced Counter that is designed to fulfill the Inventory protocol"""
 
     def __lt__(self, other: Mapping) -> bool:
@@ -125,32 +125,32 @@ class EnhancedCounter(Counter, Inventory):  # pylint: disable=abstract-method
         """
         return self == other or self > other
 
-    def __add__(self, other: Mapping) -> EnhancedCounter:
+    def __add__(self, other: Mapping) -> Bag:
         """This method is already defined by Counter, but in a un-desired method,
         so we directly tie it to Counter.update (desired),
         while also enhancing the type signature
         """
-        out = EnhancedCounter()
+        out = Bag()
         out += self
         out += other
         return out
 
-    def __sub__(self, other: Mapping) -> EnhancedCounter:
+    def __sub__(self, other: Mapping) -> Bag:
         """This method is already defined by Counter, but in a un-desired method,
         so we directly tie it to Counter.subtract (desired),
         while also enhancing the type signature
         """
-        out = EnhancedCounter()
+        out = Bag()
         out -= self
         out -= other
         return out
 
-    def __iadd__(self, other: Mapping) -> EnhancedCounter:
+    def __iadd__(self, other: Mapping) -> Bag:
         """"Inplace add, based on this objects .__add__"""
         self.update(other)
         return self
 
-    def __isub__(self, other: Mapping) -> EnhancedCounter:
+    def __isub__(self, other: Mapping) -> Bag:
         """Inplace sub, based on this objects .__sub__"""
         self.subtract(other)
         return self
@@ -172,6 +172,19 @@ class Machine:  # name not final: producer? etc
     intake: Inventory
     output: Inventory
 
+    def operable(self) -> bool:
+        """Checks whether there are sufficient resources in
+        this Machine's intake to perform the recipe
+        """
+        return self.intake > self.recipe.ingredients
+
+    def operate(self) -> None:
+        """Performs the recipe, regardless of whether
+        the intake contains enough (positive) resources
+        """
+        self.intake -= self.recipe.ingredients
+        self.output += self.recipe.products
+
     def update(self) -> None:
         """Updates the machine, performs the recipe,
         if sufficient resources are in input inventory,
@@ -180,3 +193,51 @@ class Machine:  # name not final: producer? etc
         if self.intake > self.recipe.ingredients:
             self.intake -= self.recipe.ingredients
             self.output += self.recipe.products
+
+
+class Factory:
+    """Contains multiple machines and manages the connections between them"""
+
+    def __init__(self) -> None:
+
+        # Define the machines collection as a sequence
+        # A sequence is concrete enough to provide the neccessary capability
+        # e.g. access random machines, list them, etc
+        self.machines: MutableSequence[Machine] = []
+
+    def update(self) -> None:
+        """Updates the factory, which primarily updates each machine in it
+        Ideally, update would be done carefully so that each machine
+        theoretically updates simultaneously (important when they are chained)
+        """
+        # This two-pass method is done so that whether a machine can operate is independent
+        # of other machines. This simulates simultaneous operation. There is the chance that this
+        # causes negative resources in an inventory, but this isnt actually a huge problem,
+        # as connected recipes just wont work until there is enough again
+
+        # Check which machines can run
+        updaters: MutableSequence[Machine] = []
+        for machine in self.machines:
+            if machine.operable():
+                updaters.append(machine)
+        # Run all the machines that can
+        for machine in updaters:
+            machine.operate()
+
+    def add_machine(self, machine: Machine) -> None:
+        """Adds a machine to the factory"""
+        self.machines.append(machine)
+
+
+# Design/balance specific objects, not infrastructure
+# consider transforming this into some sort of config file, which is then
+# loaded into the Recipe/Bag datastructures
+recipes = {
+    "collection": Recipe(ingredients=Bag(), products=Bag({Resource.MATERIAL: 1})),
+    "combustion": Recipe(
+        ingredients=Bag({Resource.MATERIAL: 3}), products=Bag({Resource.ENERGY: 1})
+    ),
+    "extraction": Recipe(
+        ingredients=Bag({Resource.ENERGY: 2}), products=Bag({Resource.MATERIAL: 9})
+    ),
+}
