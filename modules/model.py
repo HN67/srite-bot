3b458ed1d64832ea68cbc269321f532e2e49e63c
HN67 <hn67allardkids@gmail.com>
2023-05-18 20:59:00 -0600

    Add Guild data model with roles component

--- modules/model.py
+++ modules/model.py
@@ -103,6 +103,71 @@ class User:
                 json.dump(data, file)
 
 
+class Guild:
+    """Model to represent SriteBot data for a specific server."""
+
+    def __init__(self, guild: discord.Guild) -> None:
+        """Create a Guild based on a discord.Guild object."""
+        self.guild: discord.Guild = guild
+        self.id: int = guild.id
+        # Guild data is stored in a folder based on its id
+        self.path: str = data_path(f"guild/{str(self.id)}")
+        self.info_path: str = self.location("info.json")
+        self.roles_path: str = self.location("roles.json")
+
+    def location(self, path: object) -> str:
+        """Compose this Guild's data folder with the given path."""
+        return f"{self.path}/{path}"
+
+    def verify(self) -> None:
+        """Verify the stored data integrity for this Guild."""
+        # Verify folder existance
+        if not Path(self.path).is_dir():
+            Path(self.path).mkdir(parents=True, exist_ok=True)
+
+        # Update info file
+        with open(self.info_path, "w", encoding="utf-8") as file:
+            json.dump({"id": self.id, "name": self.guild.name}, file)
+
+    def verify_roles(self) -> None:
+        """Verify the stored role data integrity for this Guild."""
+        # Verify guild structure
+        self.verify()
+
+        # Try loading existing roles file
+        try:
+            with open(self.roles_path, "r", encoding="utf-8") as file:
+                roles_data = json.load(file)
+        except FileNotFoundError:
+            roles_data = {}
+
+        # Verify file integrity
+        self_roles_id = "self_roles"
+        if self_roles_id not in roles_data:
+            roles_data[self_roles_id] = []
+
+        with open(self.roles_path, "w", encoding="utf-8") as file:
+            json.dump(roles_data, file)
+
+    @contextmanager
+    def open_roles(self) -> Generator[dict, None, None]:
+        """Manipulate roles data within a context.
+
+        Changes to the data are automatically saved on context exit.
+        """
+        # Verify data
+        self.verify_roles()
+        # Load data
+        with open(self.roles_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        try:
+            yield data
+        finally:
+            # Resave data on context exit
+            with open(self.roles_path, "w", encoding="utf-8") as file:
+                json.dump(data, file)
+
+
 class Stocks:
     """Model to represent SriteBot stock data.
 
